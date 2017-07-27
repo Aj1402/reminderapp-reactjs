b8cb08670eef4fe1e0ab5a338a751af4c36e0169
Arjun Joshi <itscoolarjun@hotmail.com>
2017-07-28 00:55:35 +0530

    Added moment library, diplaying time until reminder.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 class App extends Component {
   constructor(props){
@@ -28,7 +29,10 @@ class App extends Component {
           reminders.map((reminder) => {
             return (
               <li key={reminder.id} className="list-group-item">
-                <div className="list-item">{reminder.text}</div>
+                <div className="list-item">
+                  <div>{reminder.text}</div>
+                  <div><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                </div>
                 <div className="list-item delete-button"
                   onClick = {() => this.deleteReminder(reminder.id)}
                   >
